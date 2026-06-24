27c4a6f7013e063569b7f11101e6c83a5d6d012f
phamvanbesau71-glitch <phamvanbesau71@gmail.com>
2026-06-24 15:04:37 +0700

    app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 # Cấu hình trang web của ứng dụng
 st.set_page_config(page_title="App Tính Thuế TNCN Việt Nam 2026", page_icon="💰", layout="centered")
 # --- CHÈN LOGO THEO FILE TRỰC TIẾP ---
-st.image("images.jpe")
+st.image("images.jpeg")
 # --- THÔNG TIN THÀNH VIÊN VÀ ĐỀ TÀI ---
-st.markdown("### 📝 ** Nguyễn Minh Tâm **")
+st.markdown("### 📝 ** Phạm Văn Bé Sáu **")
 st.title("💰 Ứng Dụng Tính Thuế Thu Nhập Cá Nhân")
 st.write("Cập nhật đầy đủ Lương, Thưởng, Tăng ca, Phụ cấp theo luật thuế mới nhất năm 2026")
 st.markdown("---")
